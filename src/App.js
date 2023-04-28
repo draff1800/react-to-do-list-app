ffda94c26c1e07a628dace6b9037214c9ec536a4
Dylan Rafferty <draff1800@gmail.com>
2023-04-27 23:24:35 -0400

    Most of ES5 > ES6 and hooks reworking done...

--- src/App.js
+++ src/App.js
@@ -1,140 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      items: [],
-      text: ""
-    };
-    
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleAddItem = this.handleAddItem.bind(this);
-    this.markItemCompleted = this.markItemCompleted.bind(this);
-    this.handleDeleteItem = this.handleDeleteItem.bind(this);
+const TodoApp = () => {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
   }
 
-  handleTextChange(event) {
-    this.setState({
-      text: event.target.value
-    });
-  }
-
-  handleAddItem(event) {
+  const handleAddItem = (event) => {
     event.preventDefault();
-    
-    var newItem = {
+
+    let newItem = {
       id: Date.now(),
-      text: this.state.text,
+      text: text,
       done: false
     };
-    
-    this.setState((prevState) => ({
-      items: prevState.items.concat(newItem),
-      text: ""
-    }));
+
+    setItems(items.concat(newItem));
+    setText("");
   }
 
-  markItemCompleted(itemId) {
-    var updatedItems = this.state.items.map(item => {
+  const markItemCompleted = (itemId) => {
+    let updatedItems = items.map(item => {
       if (itemId === item.id)
         item.done = !item.done;
-      
+
       return item;
     });
-    
+
     // State Updates are Merged
-    this.setState({
-      items: [].concat(updatedItems)
-    });
+    setItems(updatedItems);
   }
 
-  handleDeleteItem(itemId) {
-    var updatedItems = this.state.items.filter(item => {
+  const handleDeleteItem = (itemId) => {
+    let updatedItems = items.filter(item => {
       return item.id !== itemId;
     });
-    
-    this.setState({
-      items: [].concat(updatedItems)
-    });
+
+    setItems([].concat(updatedItems));
   }
 
-  render() {
-    return (
-      <div>
-        <h3 className="apptitle">TO DO LIST</h3>
-        <div className="row">
-          <div className="col-md-3">
-            <TodoList items={this.state.items} onItemCompleted={this.markItemCompleted} onDeleteItem={this.handleDeleteItem} />
-          </div>
+  return (
+    <div>
+      <h3 className="apptitle">TO DO LIST</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <TodoList items={items} onItemCompleted={markItemCompleted} onDeleteItem={handleDeleteItem} />
         </div>
-        <form className="row">
-          <div className="col-md-3">
-            <input type="text" className="form-control" onChange={this.handleTextChange} value={this.state.text} />
-          </div>
-          <div className="col-md-3">
-            <button className="btn btn-primary" onClick={this.handleAddItem} disabled={!this.state.text}>{"Add #" + (this.state.items.length + 1)}</button>
-          </div>
-        </form>
       </div>
-    );
-  }
+      <form className="row">
+        <div className="col-md-3">
+          <input type="text" className="form-control" onChange={handleTextChange} value={text} />
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-primary" onClick={handleAddItem} disabled={!text}>{"Add #" + (items.length + 1)}</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.markCompleted = this.markCompleted.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
+const TodoItem = (props) => {
+  const [listItem, setListItem] = useState(null);
+
+  const markCompleted = (event) => {
+    props.onItemCompleted(props.id);
   }
 
-  markCompleted(event) {
-    this.props.onItemCompleted(this.props.id);
-  }
-
-  deleteItem(event) {
-    this.props.onDeleteItem(this.props.id);
+  const deleteItem = (event) => {
+    props.onDeleteItem(props.id);
   }
 
   // Highlight newly added item for several seconds.
-  componentDidMount() {
-    if (this._listItem) {
+  useEffect(() => {
+    if (listItem) {
       // 1. Add highlight class.
-      this._listItem.classList.add("highlight");
+      listItem.classList.add("highlight");
 
       // 2. Set timeout.
-      setTimeout((listItem) => {
+      setTimeout(() => {
         // 3. Remove highlight class.
         listItem.classList.remove("highlight");
-      }, 500, this._listItem);
-    }
-  }
+      }, 500);
+    };
+  }, [listItem]);
 
-  render() {
-    var itemClass = "form-check todoitem " + (this.props.completed ? "done" : "undone");
-    return (
-      <li className={itemClass} ref={li => this._listItem = li }>
-        <label className="form-check-label">
-          <input type="checkbox" className="form-check-input" onChange={this.markCompleted} /> {this.props.text}
-        </label>
-        <button type="button" className="btn btn-danger btn-sm" onClick={this.deleteItem}>x</button>
-      </li>
-    );
-  }
+  let itemClass = "form-check todoitem " + (props.completed ? "done" : "undone");
+
+  return (
+    <li className={itemClass} ref={li => setListItem(li)}>
+      <label className="form-check-label">
+        <input type="checkbox" className="form-check-input" onChange={markCompleted} /> {props.text}
+      </label>
+      <button type="button" className="btn btn-danger btn-sm" onClick={deleteItem}>x</button>
+    </li>
+  );
 }
 
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul className="todolist">
-        {this.props.items.map(item => (
-          <TodoItem key={item.id} id={item.id} text={item.text} completed={item.done} onItemCompleted={this.props.onItemCompleted} onDeleteItem={this.props.onDeleteItem} />
-        ))}
-      </ul>
-    );
-  }
+const TodoList = (props) => {
+  return (
+    <ul className="todolist">
+      {props.items.map(item => (
+        <TodoItem key={item.id} id={item.id} text={item.text} completed={item.done} onItemCompleted={props.onItemCompleted} onDeleteItem={props.onDeleteItem} />
+      ))}
+    </ul>
+  );
 }
 
 // ReactDOM.render(<TodoApp />, document.getElementById("app")); // Put this back in on CodePen!
